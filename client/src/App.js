2328f7e9e180eae43c2c89441bd7c99c6da1641b
Casper Groenenberg <c.groenenberg@hotmail.com>
2018-08-27 13:15:31 +0200

    Added private routes functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import decode from 'jwt-decode';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import { Provider }  from './context';
-import Modal from './components/Modal';
 import Header from './components/layout/Header';
 import Posts from './components/Posts';
 import PostView from './components/PostView';
@@ -13,9 +13,34 @@ import Register from './components/Register';
 import Login from './components/Login';
 import Account from './components/Account';
 import NotFound from './components/NotFound';
-import AuthToggle from './components/AuthToggle';
 import { Consumer } from './context';
 
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  try {
+    decode(token);
+  } catch (err) {
+    return false;
+  }
+  return true;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      (isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+          }}
+        />
+      ))}
+  />
+);
 
 //Check if logged in and set context state
 //TODO TRY TO GET CONTEXT API IN THIS TOP LEVEL COMPONENT
@@ -23,7 +48,6 @@ export default class App extends Component {
   componentDidMount() {
     // const { dispatch } = this.props.context;
     const token = localStorage.getItem('token');
-    
     if(token) {
       // dispatch({ type: 'AUTHENTICATED'})
     };
@@ -39,7 +63,7 @@ export default class App extends Component {
             <Route exact path="/" component={Posts} />
             <Route exact path="/r/:sub" component={Posts} />
             <Route exact path="/post/:id" component={PostView} />
-            <Route exact path="/r/:sub/addpost" component={AddPost} />
+            <PrivateRoute exact path="/r/:sub/addpost" component={AddPost} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/account" component={Account} />
